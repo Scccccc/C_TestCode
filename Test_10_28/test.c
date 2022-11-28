befa436c24080de1f6e5c97dc446009acca81b3f
Sccccc <yishengaaaa@gmail.com>
2022-11-28 07:23:26 +0200

    Keep going

---
+++ Test_10_28/test.c
@@ -0,0 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <stdio.h>
+#include <string.h>
+
+//
+//int main()
+//
+//{
+//
+//	char ch1[] = { 'a', 'b', 'c' };
+//
+//	char ch2[] = "abc";
+//
+//	int len1 = strlen(ch1);
+//	int len2 = strlen(ch2);
+//
+//	printf("%d\n", len1); 
+//	printf("%d\n", len2);
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//
+//{
+//	int input = 0;
+//
+//	printf("will you hard to study\?(1 or 0)\n");
+//
+//
+//	scanf("%d", &input);
+//
+//
+//	if (input == 1)
+//	{
+//		printf("Good offer\n");
+//	}
+//	else if (input == 0)
+//	{
+//		printf("bad Job\n");
+//	}
+//	else
+//		printf("retry\n");
+//	
+//
+//
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+//
+//int main()
+//{
+//
+//	int day = 0;
+//
+//	while (day <= 365)
+//	{
+//		printf("study:%d\n", day);
+//			day++;
+//	}
+//	if (day >= 365)
+//		printf("Good offer\n");
+//	else
+//		printf("No job\n");
+//
+//	return 0;
+//}
+
+
+
