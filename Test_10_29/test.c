698779c23a1469ddd856b1c5886fb7b3a39c8cd5
Sccccc <yishengaaaa@gmail.com>
2022-11-29 07:51:01 +0200

    some code

---
+++ Test_10_29/test.c
@@ -0,0 +1,100 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <stdio.h>
+
+//
+//int main()
+//{
+//
+//	printf("\q");
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//    int M = 0;
+//
+//    M >= 1 && M <= 100;
+//
+//    scanf("%d", &M);
+//    if (M % 5 == 0)
+//        printf("YES\n");
+//    else
+//        printf("NO\n");
+//
+//
+//
+//    return 0;
+//}
+
+
+//int main()
+//{
+//    int x = 0;
+//    int y = 0;
+//    scanf("%d", &x);
+//    if (x < 0)
+//    {
+//        y == 1;
+//        printf("%d\n", y);
+//        if (x > 0)
+//            y == -1;
+//        printf("%d\n", y);
+//
+//        if (x = 0)
+//            y == 0;
+//        printf("%d\n", y);
+//    }
+//
+//    return 0;
+//}
+
+//int main()
+//
+//{
+//
+//    int x = 0;
+//    int y = 0;
+//    scanf("%d", &x);
+//    if (x < 0)
+//    {
+//        y = 1;
+//    }
+//    if (x > 0)
+//    {
+//        y = -1;
+//    }
+//    if (x = 0)
+//    {
+//        y = 0;
+//    }
+//    printf("%d\n", y);
+//
+//    return 0;
+//}
+
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("%d %d", a / b, a % b);
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    scanf("%d %d", &a, &b);
+//    int m = a / b;
+//    int n = a % b;
+//    printf("%d %d\n", m, n);
+//
+//    return 0;
+//}
