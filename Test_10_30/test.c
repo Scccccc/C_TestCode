d652fc1680da1eaf4a6a51d570274375df068cec
Sccccc <yishengaaaa@gmail.com>
2022-11-30 18:36:35 +0200

    practice questions

---
+++ Test_10_30/test.c
@@ -0,0 +1,176 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+//int main()
+//{
+//	int iq = 0;
+//	while (scanf("%d", &iq) != EOF)
+//	{
+//		if (iq >= 140)
+//			printf("Genius\n");
+//	}
+//	return 0;
+//}
+
+//
+//add(int x, int y)
+//{
+//	return (x + y);
+//}
+//
+//
+//int main()
+//{
+//
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	scanf("%d %d", &a, &b);
+//	sum = add(a, b);
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
+
+
+//
+//int main()
+//{
+//	
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%d ", arr[i]);
+//		i++;
+//	}
+//	return 0;
+////}
+//
+//int main()
+//{
+//	int n = 10;
+//	int arr[n] = { 0 };//括号内要用常量表达式
+//	return 0;
+//}
+
+//SON(int x, int y)
+//{
+//	int n = 0;
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//
+//
+//int main()
+//{
+//
+//	int a = 0;
+//	int b = 0;
+//	int biger = 0;
+//	scanf("%d %d", &a, &b);
+//
+//	biger = SON(a, b);
+//	printf("%d\n", biger);
+//
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    scanf("%d %d", &a, &b);
+//
+//    if (a > b)
+//        printf("%d>%d\n", a, b);
+//    else if (a < b)
+//        printf("%d<%d\n", a, b);
+//    else
+//        printf("%d=%d", a, b);
+//    return 0;
+//}
+
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//
+//    while (scanf("%d %d", &a, &b) != EOF)
+//    {
+//        if (a > b)
+//            printf("%d>%d\n", a, b);
+//        else if (a == b)
+//            printf("%d=%d\n", a, b);
+//        else
+//            printf("%d<%d", a, b);
+//    }
+//    return 0;
+//}
+
+//
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//
+//    while (scanf("%d\n%d", &a, &b) != EOF)
+//    {
+//        (a % b == 0);
+//            printf("Even\n");
+//            (a % b != 0);
+//            printf("Odd\n");
+//    }
+//    return 0;
+//}
+
+
+
+//KiKi想知道一个整数的奇偶性，请帮他判断。从键盘任意输入一个整数（范围-231~231-1），编程判断它的奇偶性。
+
+//int main()
+//{
+//    int num = 0;
+//
+//    while (scanf("%d", &num) != EOF)
+//    {
+//        if (num % 2 == 1)
+//            printf("Odd\n");
+//        else
+//            printf("Even\n");
+//    }
+//    return 0;
+//}
+//
+
+//int main()
+//{
+//    int sco = 0;
+//    while (scanf("%d", &sco) != EOF)
+//    {
+//        if (sco >= 60)
+//            printf("Pass\n");
+//        else
+//            printf("Fail\n");
+//    }
+//    return 0;
+//}
+
+
+
+//int main()
+//{
+//    int sco = 0;
+//    while (scanf("%d", &sco) != EOF)
+//    {
+//        if (sco >= 90 && sco <= 100)
+//            printf("Perfect\n");
+//    }
+//    return 0;
+//}
