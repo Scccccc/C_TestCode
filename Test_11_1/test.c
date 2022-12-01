33783cd7b8e3e04eace1c4e9e220e25cf065ae81
Sccccc <yishengaaaa@gmail.com>
2022-12-01 11:23:49 +0200

    practice questions

---
+++ Test_11_1/test.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//Max(int x, int y)
+//{
+//	scanf("%d %d", &x, &y);
+//	if (x > y)
+//
+//		printf("%d\n", x);
+//	else
+//		printf("%d\n", y);
+//	return 0;
+//
+//	
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int m = Max(a, b);
+//	printf("%d\n", m);
+//
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int m = (a > b ? a : b);
+//	printf("%d\n", m);
+//
+//
+//	return 0;
+//}
+
+
