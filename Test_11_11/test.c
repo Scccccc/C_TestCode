317abdf9c667dc78205e5db8cf173594592c7198
Sccccc <yishengaaaa@gmail.com>
2022-12-12 16:23:14 +0200

    keep going

---
+++ Test_11_11/test.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+//		{
+//			printf("YES\n");
+//		}
+//		else
+//		{
+//			printf("NO\n");
+//		}
+//		getchar();
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	char n = 0;
+//	scanf("%c", &n);
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 5 - 1 - i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("%c ", n);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
