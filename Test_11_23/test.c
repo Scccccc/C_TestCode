4b9af1db36e8476e769f3f8281fd8b41e162e9ea
Sccccc <yishengaaaa@gmail.com>
2023-01-08 13:33:56 +0200

    Keep going

---
+++ Test_11_23/test.c
@@ -0,0 +1,113 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//	int** ppa = &pa;
+//
+//	printf("%d\n", **ppa);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	int d = 40;
+//	int e = 50;
+//	int* arr[5] = { &a, &b, &c, &d, &e };
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[] = { 1,2,3,4 };
+//	int b[] = { 5,6,7,8, };
+//	int c[] = { 8,9,10,11 };
+//	int* arr[] = { a, b, c };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = n / 10 % 10;
+//	printf("%d\n", ret);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d\n", n);
+//	return 0;
+//}
+
+//int main()
+//{
+//	float n = 0;
+//	scanf("%f", &n);
+//	printf("%.3f\n", n);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char ch = 0;
+//	ch = getchar();
+//	printf("%c\n", ch);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	while (~scanf("%d %d %d", &a, &b, &c))
+//	{
+//		printf("%d\n", b);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char ch = 0;
+//	while (~scanf("%c", &ch))
+//	{
+//		int i = 0;
+//		for (i = 0; i < 3; i++)
+//		{
+//			int j = 0;
+//			for (j = 0; j < 3; j++)
+//			{
+//				printf("%c", ch);
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
