648eba3db263d739e666a47572766a2cdee7ecb8
Sccccc <yishengaaaa@gmail.com>
2023-01-14 17:36:15 +0200

    Keep going

---
+++ Test_12_5/test.c
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//
+#include <stdio.h>
+//
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int ret = 0;
+//	ret = Add(a, b);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//KiKi喝酸奶
+//int main()
+//{
+//    int n = 0;
+//    int h = 0;
+//    int m = 0;
+//    int sum = 0;
+//    while (scanf("%d %d %d", &n, &h, &m) != EOF)
+//    {
+//        if (m <= n * h)
+//        {
+//            sum = (m / h) + (m % h);
+//        }
+//        printf("%d\n", n - sum);
+//    }
+//
+//    return 0;
+//}
+
+//温度转换
+//
+//int main()
+//{
+//    double f = 0.0;
+//    scanf("%lf", &f);
+//    double c = 0.0;
+//    c = 5.0 / 9 * (f - 32.0);
+//    printf("%0.3lf", c);
+//    return 0;
+//}
+
+
+//数字三角形
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) != EOF)
+//	{
+//		int i = 0;
+//		for (i = 1; i <= n; i++)
+//		{
+//			int j = 0;
+//			for (j = 1; j <= i; j++)
+//			{
+//				printf("%d ", j);
+//			}
+//			printf("\n");
+//		}
+//	}
+//
+//	return 0;
+//}
