d047b0896b26c022c763c3b19008dc12d1361b67
Sccccc <yishengaaaa@gmail.com>
2023-01-17 16:30:15 +0200

    keep going

---
+++ Test_1_3/test.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+//判断大小端字节序
+//int check_sys(int a)
+//{
+//	return *(char*)&a;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int ret = check_sys(a);
+//	if (ret > 0)
+//	{
+//		printf("little\n");
+//	}
+//	else
+//	{
+//		printf("big\n");
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char a = -128;
+//	printf("%u\n", a);
+//	return 0;
+//}
